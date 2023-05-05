cd2e88024f09bcdd150adbdd04f410329708d290
Petar Barać <petarbarac777@gmail.com>
2023-05-05 23:12:30 +0200

    .

--- app/src/main/java/com/intelektualcicii/todoapp/Activity/HomeActivity.java
+++ app/src/main/java/com/intelektualcicii/todoapp/Activity/HomeActivity.java
@@ -6,16 +6,21 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.intelektualcicii.todoapp.R;
 
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     Button logOut;
+    FloatingActionButton addTask;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -23,7 +28,13 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         logOut=findViewById(R.id.bt_logOut);
+        Spinner spinner = findViewById(R.id.category_spinner);
+        addTask=findViewById(R.id.add_task_floating_bt);
 
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.categories_hardcoded, R.layout.dropdown_item);
+        adapter.setDropDownViewResource(R.layout.dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
 
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +45,26 @@ public class HomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+        
+        addTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(HomeActivity.this, "Add to-do clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+    }
+    
+    
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
