1e64dc17be51cef37afe5c11ef7483ed944a2966
Petar Barać <petarbarac777@gmail.com>
2023-05-12 15:26:16 +0200

    implemendted createTODO design

--- app/src/main/java/com/intelektualcicii/todoapp/Dialog/CreateNewTaskBottomSheet.java
+++ app/src/main/java/com/intelektualcicii/todoapp/Dialog/CreateNewTaskBottomSheet.java
@@ -1,9 +1,12 @@
 package com.intelektualcicii.todoapp.Dialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,9 +20,23 @@ public class CreateNewTaskBottomSheet extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.bottom_sheet_create_new_task,container,false);
-        return view;
 
+
+
+
+
+
+
+
+        return view;
 
     }
 
+
+    @Override
+    public void onDismiss(@NonNull DialogInterface dialog) {
+        super.onDismiss(dialog);
+        Toast.makeText(getContext(), "ToDO saved lol", Toast.LENGTH_SHORT).show();
+        //implemetiraj da se savea TO-DO
+    }
 }
