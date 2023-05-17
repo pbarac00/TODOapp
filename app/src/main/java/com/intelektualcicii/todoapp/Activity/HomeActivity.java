07615b01054ea9a43186b284bac4a930c372f8e7
Petar Barać <petarbarac777@gmail.com>
2023-05-17 14:40:25 +0200

    .

--- app/src/main/java/com/intelektualcicii/todoapp/Activity/HomeActivity.java
+++ app/src/main/java/com/intelektualcicii/todoapp/Activity/HomeActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -34,6 +35,8 @@ public class HomeActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TaskAdapter taskAdapter;
     TaskDatabase taskDatabase;
+    ImageView sortPriority,sortAZ,sortDate;
+    Boolean isSortByPrioriyClicked, isSortByAzClicked,isSortByDateClicked;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -42,9 +45,15 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         logOut=findViewById(R.id.bt_logOut);
         addTask=findViewById(R.id.add_task_floating_bt);
+        sortPriority=findViewById(R.id.iv_priortity_home);
+        sortAZ=findViewById(R.id.iv_sort_az_home);
+        sortDate=findViewById(R.id.iv_sortByDate_home);
 
         recyclerView=findViewById(R.id.recyclerViewHomeTasks);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        isSortByPrioriyClicked=false;
+        isSortByAzClicked=false;
+        isSortByDateClicked=false;
 
         taskDatabase= Room.databaseBuilder(getApplicationContext(),
                         TaskDatabase.class,"task").
@@ -77,6 +86,53 @@ public class HomeActivity extends AppCompatActivity {
                 createNewTaskBottomSheet.show(getSupportFragmentManager(),"createNewTaskBottomSheet");
             }
         });
+
+        sortPriority.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSortByPrioriyClicked)
+                {
+                    isSortByPrioriyClicked=false;
+                    sortPriority.setImageResource(R.drawable.priority);
+                }
+                else{
+                    isSortByPrioriyClicked=true;
+                    sortPriority.setImageResource(R.drawable.priority_blue);
+                }
+            }
+        });
+
+
+        sortDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSortByDateClicked)
+                {
+                    isSortByDateClicked=false;
+                    sortDate.setImageResource(R.drawable.deadline);
+                }
+                else{
+                    isSortByDateClicked=true;
+                    sortDate.setImageResource(R.drawable.deadline_blue);
+                }
+            }
+        });
+
+        sortAZ.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSortByAzClicked)
+                {
+                    isSortByAzClicked=false;
+                    sortAZ.setImageResource(R.drawable.sort_az);
+                }
+                else{
+                    isSortByAzClicked=true;
+                    sortAZ.setImageResource(R.drawable.sort_az_blue);
+                }
+            }
+        });
+
         
     }
     
