421538a26445aef3af298878a893615195735f72
Petar Barać <petarbarac777@gmail.com>
2023-06-07 14:03:43 +0200

    .

---
+++ app/src/main/java/com/intelektualcicii/todoapp/Activity/EditTaskActivity.java
@@ -0,0 +1,14 @@
+package com.intelektualcicii.todoapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.intelektualcicii.todoapp.R;
+
+public class EditTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_task);
+    }
+}
--- app/src/main/java/com/intelektualcicii/todoapp/Activity/TaskDetailActivity.java
+++ app/src/main/java/com/intelektualcicii/todoapp/Activity/TaskDetailActivity.java
@@ -2,15 +2,30 @@ package com.intelektualcicii.todoapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.intelektualcicii.todoapp.R;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class TaskDetailActivity extends AppCompatActivity {
 
+    ImageView editTask;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_detail);
+        editTask=findViewById(R.id.iv_editTask);
 
+        editTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(TaskDetailActivity.this,EditTaskActivity.class));
+            }
+        });
 
     }
 }
